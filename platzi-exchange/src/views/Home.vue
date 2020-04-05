4373d93870972d4a37f0908123d1f0f71732f0e9
Snows02 <nieves.ardila02@gmail.com>
2020-04-04 19:11:46 -0500

    Agregando views y componente

---
+++ platzi-exchange/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <px-assests-table />
+  </div>
+</template>
+
+<script>
+import PxAssestsTable from "@/components/PxAssestsTable";
+
+export default {
+  name: "Home",
+
+  components: { PxAssestsTable }
+};
+</script>
